43565df551700e2cf29843d4af684a55037e864b
Lachlan MacPhee <github@lachlanmacphee.com>
2025-12-08 00:57:19 +1100

    fix: use sveltekit building helper instead of proxy

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,22 +1,25 @@
+import { building } from '$app/environment';
 import { drizzle, type NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { Resource } from 'sst';
 import * as schema from './schema';
 
-let _db: NodePgDatabase<typeof schema> | null = null;
+let _db:
+	| (NodePgDatabase<typeof schema> & {
+		$client: Pool;
+	})
+	| null = null;
 
-export const db = new Proxy({} as NodePgDatabase<typeof schema>, {
-	get(_, prop) {
-		if (!_db) {
-			const pool = new Pool({
-				host: Resource.Database.host,
-				port: Resource.Database.port,
-				user: Resource.Database.username,
-				password: Resource.Database.password,
-				database: Resource.Database.database
-			});
-			_db = drizzle(pool, { schema });
-		}
-		return Reflect.get(_db, prop);
-	}
-});
+if (!building) {
+	const pool = new Pool({
+		host: Resource.Database.host,
+		port: Resource.Database.port,
+		user: Resource.Database.username,
+		password: Resource.Database.password,
+		database: Resource.Database.database
+	});
+
+	_db = drizzle(pool, { schema });
+}
+
+export const db = _db!;
--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,40 +1,29 @@
+import { building } from '$app/environment';
 import { Google, MicrosoftEntraId } from 'arctic';
 import { Resource } from 'sst';
 
 let _google: Google | null = null;
-export function getGoogle(): Google {
-	if (!_google) {
-		_google = new Google(
-			Resource.GoogleClientID.value,
-			Resource.GoogleClientSecret.value,
-			'http://localhost:5173/login/google/callback'
-		);
-	}
-	return _google;
-}
-
 let _microsoft: MicrosoftEntraId | null = null;
-export function getMicrosoft(): MicrosoftEntraId {
-	if (!_microsoft) {
-		_microsoft = new MicrosoftEntraId(
-			Resource.MicrosoftTenantID.value,
-			Resource.MicrosoftClientID.value,
-			Resource.MicrosoftClientSecret.value,
-			'http://localhost:5173/login/microsoft/callback'
-		);
-	}
-	return _microsoft;
+
+if (!building) {
+	const isProd = ['production'].includes(Resource.App?.stage);
+	_google = new Google(
+		Resource.GoogleClientID.value,
+		Resource.GoogleClientSecret.value,
+		isProd
+			? 'https://eddi.com.au/login/google/callback'
+			: 'http://localhost:5173/login/google/callback'
+	);
+
+	_microsoft = new MicrosoftEntraId(
+		Resource.MicrosoftTenantID.value,
+		Resource.MicrosoftClientID.value,
+		Resource.MicrosoftClientSecret.value,
+		isProd
+			? 'https://eddi.com.au/login/microsoft/callback'
+			: 'http://localhost:5173/login/microsoft/callback'
+	);
 }
 
-// Keep backwards-compatible exports using getters
-export const google = new Proxy({} as Google, {
-	get(_, prop) {
-		return Reflect.get(getGoogle(), prop);
-	}
-});
-
-export const microsoft = new Proxy({} as MicrosoftEntraId, {
-	get(_, prop) {
-		return Reflect.get(getMicrosoft(), prop);
-	}
-});
+export const google = _google;
+export const microsoft = _microsoft;
