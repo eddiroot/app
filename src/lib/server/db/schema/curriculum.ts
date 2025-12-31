fd0e893b05b60fd911aa70bf9bfc556f767ab4e0
JackMoses3 <mosesjack@gmail.com>
2025-12-31 11:53:40 +1100

    fix: Refactoring seeding setup

--- src/lib/server/db/schema/curriculum.ts
+++ src/lib/server/db/schema/curriculum.ts
@@ -1,8 +1,8 @@
 
+import { gradeScaleEnum } from '$lib/enums';
 import { boolean, doublePrecision, index, integer, pgEnum, pgTable, text, unique, varchar } from 'drizzle-orm/pg-core';
-import { gradeScaleEnum, yearLevelEnum } from '../../../enums';
 import { school } from './schools';
-import { embeddings, publish, timestamps } from './utils';
+import { embeddings, publish, timestamps, yearLevelEnumPg } from './utils';
 
 export const curriculum = pgTable('crclm', {
 	id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
@@ -70,23 +70,6 @@ export const learningAreaContent = pgTable('lrn_a_cont', {
 
 export type LearningAreaContent = typeof learningAreaContent.$inferSelect;
 
-export const yearLevelEnumPg = pgEnum('enum_year_level', [
-	yearLevelEnum.none,
-	yearLevelEnum.foundation,
-	yearLevelEnum.year1,
-	yearLevelEnum.year2,
-	yearLevelEnum.year3,
-	yearLevelEnum.year4,
-	yearLevelEnum.year5,
-	yearLevelEnum.year6,
-	yearLevelEnum.year7,
-	yearLevelEnum.year8,
-	yearLevelEnum.year9,
-	yearLevelEnum.year10,
-	yearLevelEnum.year11,
-	yearLevelEnum.year12,
-]);
-
 export const learningAreaTopic = pgTable('lrn_a_topic', {
 	id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1000 }),
 	learningAreaId: integer('lrn_a_id')
@@ -98,7 +81,7 @@ export const learningAreaTopic = pgTable('lrn_a_topic', {
 	...embeddings
 	},
 	(self) => [
-		unique().on(self.name),
+		unique().on(self.learningAreaId, self.name),
 	]
 );
 
@@ -111,7 +94,7 @@ export const learningAreaStandard = pgTable('lrn_a_std', {
 		.references(() => learningArea.id, { onDelete: 'cascade' }),
 	learningAreaTopicId: integer('lrn_a_topic_id')
 		.references(() => learningAreaTopic.id, { onDelete: 'cascade' }),
-	code: varchar('code', { length: 10 }).notNull(),
+	code: varchar('code', { length: 20 }).notNull(),
 	description: text('description'),
 	yearLevel: yearLevelEnumPg().notNull(),
 	isArchived: boolean('is_archived').notNull().default(false),
@@ -297,6 +280,7 @@ export const curriculumSubjectExtraContent = pgTable('crclm_sub_cont', {
 );
 
 export type CurriculumSubjectExtraContent = typeof curriculumSubjectExtraContent.$inferSelect;
+
 
 export const gradeScaleEnumPg = pgEnum('enum_grade_scale', [
 	gradeScaleEnum.IB_CP,
